d9ef8fb0cf84eca150745f57b4ab9f1758bf3321
ad1510875353 <1510875353@qq.com>
2024-12-08 23:13:41 +0800

    简化代码

--- main/main.c
+++ main/main.c
@@ -37,7 +37,8 @@ int onDisconnect(struct ble_gap_event *event, void *arg, uint16_t conn_handle)
     sync_num = sync_num - 1;
     connstate.connected_num = connstate.connected_num - 1;
     connstate.connecting = false;
-    for (uint8_t i = 0; i < PROFILE_NUM; i++){
+    for (uint8_t i = 0; i < PROFILE_NUM; i++)
+    {
         if (conn_handle == peerinfos[i].conn_handle)
         {
             ESP_LOGE(TAG, "%s disconnect,connect handle = %d,reason = %d", peerinfos[i].name, conn_handle, event->disconnect.reason);
@@ -66,30 +67,23 @@ int onNotifyRx(struct ble_gap_event *event, void *arg, uint16_t conn_handle)
         {
             if (event->notify_rx.attr_handle == peerinfos[i].val_handle)
             {
-                if (OS_MBUF_PKTLEN(event->notify_rx.om) == 10)
-                {
-                    gettimeofday(&timeval_s, NULL);
-                    local_ts = (uint64_t)timeval_s.tv_sec * 1000000L + (uint64_t)timeval_s.tv_usec;
-                    ble_gattc_write_flat(conn_handle, peerinfos[i].val_handle, &local_ts, sizeof(local_ts), NULL, NULL);
-                    set_led_state(1);
-                    ESP_LOGE(TAG, "Get NotifyData, NotifyRX time = %lld, send to %s to sync ", local_ts, peerinfos[i].name);
-                    xSemaphoreGive(printSemaphore);
-                }
+                ble_gattc_write_flat(conn_handle, peerinfos[i].val_handle, &local_ts, sizeof(local_ts), NULL, NULL);
+                set_led_state(1);
+                ESP_LOGI(TAG, "收到通知后立刻进行写操作");
+                xSemaphoreGive(printSemaphore);
             }
             break;
         }
     return 0;
 }
 
-
-// 生成下降沿打印输出
+// 5秒后关闭led,等待下一次通知的到来
 void print_time_task(void *pvParameters)
 {
     for (;;)
     {
         if (xSemaphoreTake(printSemaphore, portMAX_DELAY))
         {
-            // 5秒后关闭led,等待下一次通知的到来
             vTaskDelay(5000);
             set_led_state(0);
         }
